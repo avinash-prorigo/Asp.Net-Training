c0efb12df0af56556d33a147a9611e6efef2d5cb
Avinash Tambe <avinashsprorigo@gmail.com>
2020-06-02 16:04:14 +0530

    - Added .cs and project files for LogParser console application.

---
+++ LogParser/src/LogParser/CsvGeneration.cs
@@ -0,0 +1,28 @@
+using System.IO;
+using System;
+using System.Collections.Generic;
+
+namespace LogParser
+{
+    public class CsvGeneration
+    {
+        public void createCsvFile(List<ProcessLogcontent> logcontents, string csvFile)
+        {
+            string delimiter = ", ";
+            string appendLogs;
+            int counter = 1;
+            if (!File.Exists(csvFile))
+            {
+                string createText = "No" + delimiter + "Level" + delimiter + "Date" + delimiter + "Time" + delimiter+"Text" + Environment.NewLine;
+                File.WriteAllText(csvFile, createText);
+            }
+            foreach (ProcessLogcontent processLogcontent in logcontents)
+            {
+                appendLogs = counter++ + delimiter + processLogcontent.Level + delimiter + processLogcontent.Date + delimiter + processLogcontent.Time +
+                delimiter + "\""+ processLogcontent.Description +"\""+ Environment.NewLine;
+                File.AppendAllText(csvFile, appendLogs);
+            }
+
+        }
+    }
+}
---
+++ LogParser/src/LogParser/ProcessLogcontent.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System;
+
+namespace LogParser
+{
+    public class ProcessLogcontent
+    {
+        public string Level;
+        public string Date;
+        public string Time;
+        public string Description;
+
+        public string getdate(string logLine)
+        {
+            string DateField = Regex.Match(logLine, @"(?<=^(\S+\s){0})\S+").ToString();
+            DateTime dt = DateTime.Parse(DateField);
+            return ($"{dt.Day} {dt.ToString("MMMM")} {dt.Year}").ToString();
+        }
+
+        public string getTime(string logLine)
+        {
+            string TimeField = Regex.Match(logLine, @"(?<=^(\S+\s){1})\S+").ToString();
+            DateTime tm = DateTime.Parse(TimeField);
+            return tm.ToShortTimeString();
+        }
+        public string getLogLevel(string logLine)
+        {
+            string logLevel = Regex.Match(logLine, @"(?<=^(\S+\s){2})\S+").ToString();
+            return logLevel;
+        }
+        public string getDescription(string logLine)
+        {
+            string[] fields = logLine.Split(":.");
+            return ":." + fields[1].Trim();
+        }
+
+        public List<ProcessLogcontent> getCsvCompliantContent(List<string> logLines)
+        {
+            List<ProcessLogcontent> CsvContents = new List<ProcessLogcontent>();
+            
+            foreach (var line in logLines)
+            {
+                try{
+                var csvContent = new ProcessLogcontent();
+                csvContent.Level = csvContent.getLogLevel(line);
+                csvContent.Date = csvContent.getdate(line);
+                csvContent.Time = csvContent.getTime(line);
+                csvContent.Description = csvContent.getDescription(line);
+
+                CsvContents.Add(csvContent);
+                }catch(Exception e){
+                    Console.WriteLine($"{e.Message}");
+                }
+            }
+
+            return CsvContents;
+        }
+    }
+}
